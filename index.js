ec6c2fc62d7bc596801878678b14e05916e296c4
Laurine Ndanu Nzuki <nzukilaurine@gmail.com>
2024-07-10 02:11:29 +0300

    All tests passing

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const clicker = document.getElementById('button');
+
 function addingEventListener() {
+    clicker.addEventListener('click', function() {
+        alert('Button clicked');
+    });
 }
+addingEventListener();
